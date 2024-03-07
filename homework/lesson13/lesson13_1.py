d1efa7b097ea808929da9ff8c66ef2cde28816fa
barik <barvinskij.d@edu.narfu.ru>
2024-03-07 22:07:35 +0300

    Выполнение 12 урока.

---
+++ homework/lesson13/lesson13_1.py
@@ -0,0 +1,19 @@
+import random
+
+a = int(input('Кол-во строк = '))
+b = int(input('Кол-во столбцов = '))
+
+m1 = [[random.randint(0, 10) for j in range(b)] for i in range(a)]
+print('Первая матрица: ')
+for i in range(a):
+    print(m1[i])
+
+m2 = [[random.randint(0, 10) for j in range(b)] for i in range(a)]
+print('Вторая матрица: ')
+for i in range(a):
+    print(m2[i])
+
+res = [[m1[i][j] + m2[i][j] for j in range(len(m1[0]))] for i in range(len(m1))]
+print("Результат сложения двух матриц: ")
+for n in res:
+    print(n)
